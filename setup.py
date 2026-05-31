c9a87c426db1c478c8f08d6f03381b3f325ecac1
KhanAmaan55 <amaanjk.a000@gmail.com>
2026-05-31 16:53:56 +0530

    chore: update version to 0.7.0 in configuration and setup files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="domainspyder",
-    version="0.5.0",
+    version="0.7.0",
     description="Domain Intelligence Framework",
     author="Amaan Khan",
     packages=find_packages(),
